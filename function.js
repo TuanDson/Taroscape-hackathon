46ad77c3e556ac562203871739f9716c915b71a6
Latherine <vot@dickinson.edu>
2025-04-05 17:31:06 -0400

    adding js file

---
+++ function.js
@@ -0,0 +1,27 @@
+const card_contain = new Map([
+    [0,["thefool.jpeg","The Fool","New beginnings, adventure, spontaneity, taking a leap of faith."]],
+    [1,["themagician.jpeg","The Magician", " Manifestation, creativity, skill, resourcefulness."]],
+    [2,["thehighpriestess.jpeg","The High Priestess","Intuition, wisdom, inner knowledge, mystery."]],
+    [3,["theempress.jpeg","The Empress", "Fertility, nurturing, abundance, creativity."]],
+    [4,["theemperor.jpeg","The Emperor"," Authority, structure, stability, leadership."]],
+    [5,["thehierophant.jpeg","The Hierophant", "Tradition, spirituality, guidance, learning."]],
+    [6,["TheLovers.jpg","The Lovers","Love, relationships, harmony, choices."]],
+    [7, ["thechariot.jpeg","The Chariot","Determination, willpower, success through control."]],
+    [8,["thestrength.jpeg","The Strength", "Courage, patience, inner strength, compassion."]],
+    [9,["thehermit.jpeg","The Hermit","Solitude, introspection, inner guidance, wisdom."]],
+    [10,["wheeloffortune.jpeg","Wheel of Fortune", "Luck, destiny, cycles, turning points."]],
+    [11,["justice.jpeg","Justice", "Fairness, balance, truth, legal matters."]],
+    [12,["thehangedman.jpeg","The Hanged Man","Surrender, letting go, seeing things from a different perspective."]],
+    [13,["death.jpeg","Death","Transformation, endings, new beginnings, change."]],
+    [14,["temperance.jpeg","Temperance","Balance, moderation, patience, harmony."]],
+    [15,["thedevil.jpeg","The Devil","emptation, materialism, addiction, self-imposed bondage."]],
+    [16,["thetower.jpeg","The Tower", "Sudden upheaval, destruction, awakening, revelation."]],
+    [17,["thestar.jpeg","The Star", "Hope, inspiration, spiritual guidance, healing."]],
+    [18,["themoon.jpeg","The Moon", "Illusion, subconscious, intuition, confusion."]],
+    [19,["thesun.jpeg","The Sun","Joy, success, positivity, vitality."]],
+    [20,["judgement.jpeg","Judgement", "Reflection, awakening, redemption, self-evaluation."]],
+    [21,["theworld.jpeg","The World", "Completion, wholeness, fulfillment, harmony."]]
+]);
+
+let a = Math.floor(Math.random()*22);
+console.log(a);
